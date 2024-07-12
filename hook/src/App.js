3749bdca48849b9655068a4bf24ab43e238057ed
munhyeonsuk <m01024038735@gmail.com>
2024-07-12 10:08:29 +0900

    0712 useEffect 사용 시작

--- hook/src/App.js
+++ hook/src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const bgcolor = ["red", "green", "blue"];
+  //body태그의 배경색을 바꾸고 싶음 useEffect를 통해 갈수 있음. html 상륙이후 실행. useState는 컴퍼넌트 밖으로 못나감
+  useEffect(()=>{}, [])
   return (
     <div className="App">
       <button onClick={()=>{
@@ -11,7 +14,6 @@ function App() {
 
       <button onClick={()=>{
         setCount(count -1);
-        //대입식 절대 불가 연산식만
       }}> {count}감소해줘</button>
     </div>
   );
