e668fd0b0a85245424fb2887be36e73b4f898cc5
munhyeonsuk <m01024038735@gmail.com>
2024-07-12 10:34:28 +0900

    0712 useEffect 삼항식

--- hook/src/App.js
+++ hook/src/App.js
@@ -4,11 +4,13 @@ function App() {
   const [count, setCount] = useState(0);
   const bgcolor = ["red", "green", "blue"];
   //body태그의 배경색을 바꾸고 싶음 useEffect를 통해 갈수 있음. html 상륙이후 실행. useState는 컴퍼넌트 밖으로 못나감
-  useEffect(()=>{}, [])
+  useEffect(()=>{
+    document.body.style.backgroundColor = bgcolor[count];
+  }, [count])
   return (
     <div className="App">
       <button onClick={()=>{
-        setCount(count +1);
+        setCount(count < bgcolor.length -1 ? count +1 : 0);
         //대입식 절대 불가 연산식만
       }}> {count}증가해줘</button>
 
