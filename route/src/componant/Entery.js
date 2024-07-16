60119329e47f231bc5f477607442f324a674675b
munhyeonsuk <m01024038735@gmail.com>
2024-07-16 11:41:57 +0900

    0716 useEffect

--- route/src/componant/Entery.js
+++ route/src/componant/Entery.js
@@ -1,16 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 function Entery() {
     let count = 0;
     //일반 변수 상태변수값을 받아서 출력하려면 return 출력식에서 처리하기
-    const [num, setNum] = useState(0);
+    const [num, setNum] = useState(0);//랜더링
+    useEffect(()=>{
+        document.body.style.background = 'red'
+    },[]);
+    useEffect(()=>{
+        document.querySelector('button').addEventListener('click', ()=>{
+            setNum(num + 1);
+        })
+    },[num])//랜더링 끝나고 나서 html태그로 이해
+
     return (
         <div>
-            <button onClick={()=>{
-                            setNum(num + 1);// 상태변수의 값을 변경하는 상태함수
-                            count = num; //화살표함수 내에서 저장 안됨
-                            }
-                            }>일반변수 count 하나씩 증가</button>
+            <button>일반변수 count 하나씩 증가</button>
             <h1>나는 일반변수 count : {`${count=num}`} </h1>
         </div>
     )
