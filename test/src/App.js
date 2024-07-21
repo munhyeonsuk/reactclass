afcd474b67cca9a9b3aebc2ccf94b26da7a7bd3b
munhyeonsuk <m01024038735@gmail.com>
2024-07-21 17:29:14 +0900

    삼항식 사용해 active

--- test/src/App.js
+++ test/src/App.js
@@ -1,22 +1,17 @@
 
 import './App.css';
-import React,{useEffect} from 'react';
 import { Title, Menubtn } from './component/Common';
 
 const tabmn = ["탄산", "주스", "커피차/차", "스포츠/건강음료", "생수", "탄산수"];
 
 function App() {
-  useEffect(()=>{
-    const btns = document.querySelectorAll("button")
-    btns[0].classList.add("active")
-  },[])
   return (
   <section>
   <Title> 칠성몰 인기 상품</Title>
   <div className='text-center'>
     {tabmn.map((v,i)=>{
       return(
-        <Menubtn className='mx-2'>{v}</Menubtn>
+        <Menubtn className={`mx-2 ${i===0 ? 'active' : null}`}>{v}</Menubtn>
       )
     })}
   </div>
