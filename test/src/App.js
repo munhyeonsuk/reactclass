31c52ca6455d5ddda932399d16183602adb7700e
munhyeonsuk <m01024038735@gmail.com>
2024-07-21 17:40:07 +0900

    useState로 tab 활성화

--- test/src/App.js
+++ test/src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
+import React, {useState} from 'react';
 import { Title, Menubtn } from './component/Common';
 
 const tabmn = ["탄산", "주스", "커피차/차", "스포츠/건강음료", "생수", "탄산수"];
 
 function App() {
+  const [tabnum, setTabnum] = useState(0)
+
   return (
   <section>
-  <Title> 칠성몰 인기 상품</Title>
+  <Title className='my-3 pb-3'> 칠성몰 인기 상품</Title>
   <div className='text-center'>
     {tabmn.map((v,i)=>{
       return(
-        <Menubtn className={`mx-2 ${i===0 ? 'active' : null}`}>{v}</Menubtn>
+        <Menubtn className={`mx-2 ${i===tabnum ? 'active' : null}`} onClick={()=>{setTabnum(i)}} >{v}</Menubtn>
       )
     })}
   </div>
